f4d0daf6b4a1f62d202a16f943a8beb9d9298d83
Piotr Sikora <piotrsikora@google.com>
2020-11-18 09:38:30 +0000

    Update ABI to Proxy-Wasm ABI v0.2.1.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- examples/http_auth_random.rs
+++ examples/http_auth_random.rs
@@ -36,7 +36,7 @@ impl RootContext for HttpAuthRandomRoot {
 struct HttpAuthRandom;
 
 impl HttpContext for HttpAuthRandom {
-    fn on_http_request_headers(&mut self, _: usize) -> Action {
+    fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
         self.dispatch_http_call(
             "httpbin",
             vec![
@@ -52,7 +52,7 @@ impl HttpContext for HttpAuthRandom {
         Action::Pause
     }
 
-    fn on_http_response_headers(&mut self, _: usize) -> Action {
+    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
         self.set_http_response_header("Powered-By", Some("proxy-wasm"));
         Action::Continue
     }
